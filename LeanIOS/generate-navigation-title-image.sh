5834d087e6eb6b369e3d221fb1d708cef936505a
Weiyin He <weiyin@users.noreply.github.com>
2018-04-20 12:10:26 -0400

    Use optipng instead of pngcrush.

--- LeanIOS/generate-navigation-title-image.sh
+++ LeanIOS/generate-navigation-title-image.sh
@@ -6,4 +6,4 @@ BASEDIR=$(dirname $0)
 
 sips --resampleHeight 90 -s format png --out $BASEDIR/navbar_logo.png $BASEDIR/navigationTitleImageLocation 2>&1
 
-pngcrush -q -rem allb -brute -ow $BASEDIR/navbar_logo.png 2>&1
+optipng $BASEDIR/navbar_logo.png 2>&1
