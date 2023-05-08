6f34f91a97722f68b7687d05348a23201951660e
Hunaid Hassan <hhunaid@gmail.com>
2023-05-08 15:57:05 +0200

    Fix headers

--- LeanIOS/GonativeIO-Bridging-Header.h
+++ LeanIOS/GonativeIO-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <GoNative/GoNativeAppConfig.h>
+#import <GoNativeCore/GoNativeAppConfig.h>
 #import "LEANUtilities.h"
 #import "LEANWebViewController.h"
 #import "LEANRootViewController.h"
